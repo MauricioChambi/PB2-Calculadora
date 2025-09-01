b75a8c738fe7f6e49e35af3de4b889d4904f25c3
Mauricio Chambi <chambi7mauricio@gmail.com>
2025-09-01 15:13:27 -0300

    Ejercicio resuelto calculadora

--- src/ar/edu/unlam/dominio/Calculadora.java
+++ src/ar/edu/unlam/dominio/Calculadora.java
@@ -1,6 +1,20 @@
 package ar.edu.unlam.dominio;
 
 public class Calculadora {
-	
-	
+
+	public Double sumar(Double numeroUno, Double numeroDos) {
+		return numeroUno + numeroDos;
+	}
+
+	public Double restar(Double numeroUno, Double numeroDos) {
+		return numeroUno - numeroDos;
+	}
+
+	public Double multiplicar(Double numeroUno, Double numeroDos) {
+		return numeroUno * numeroDos;
+	}
+
+	public Double dividir(Double numeroUno, Double numeroDos) {
+		return numeroDos == 0.0 ? 0.0 : numeroUno / numeroDos;
+	}
 }
--- test/ar/edu/unlam/dominio/CalculadoraTest.java
+++ test/ar/edu/unlam/dominio/CalculadoraTest.java
@@ -1,6 +1,50 @@
 package ar.edu.unlam.dominio;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculadoraTest {
 
+	@Test
+	public void dadoQueExisteUnaCalculadoraConDosNumerosPodremosHacerLaSumaDeEstos() {
+		
+		Calculadora casio = new Calculadora();
+
+		Double resultadoDeLaSuma = casio.sumar(10.0, 10.0);
+		Double resultadoTest = 20.0;
+
+		assertEquals(resultadoTest, resultadoDeLaSuma);
+	}
+	@Test
+	public void dadoQueExisteUnaCalculadoraConDosNumerosPodremosHacerLaRestarDeEstos() {
+		
+		Calculadora casio = new Calculadora();
+		
+		Double resultadoDeLaSuma = casio.restar(10.0, 10.0);
+		Double resultadoTest = 0.0;
+		
+		assertEquals(resultadoTest, resultadoDeLaSuma);
+	}
+	@Test
+	public void dadoQueExisteUnaCalculadoraConDosNumerosPodremosHacerLaMultiplicacionDeEstos() {
+		
+		Calculadora casio = new Calculadora();
+		
+		Double resultadoDeLaSuma = casio.multiplicar(10.0, 10.0);
+		Double resultadoTest = 100.0;
+		
+		assertEquals(resultadoTest, resultadoDeLaSuma);
+	}
+	@Test
+	public void dadoQueExisteUnaCalculadoraConDosNumerosPodremosHacerLaDivisionDeEstos() {
+		
+		Calculadora casio = new Calculadora();
+		
+		Double resultadoDeLaSuma = casio.dividir(10.0, 10.0);
+		Double resultadoTest = 1.0;
+		
+		assertEquals(resultadoTest, resultadoDeLaSuma);
+	}	
 	
 }
